bb402295530f1c96ab43a621ee7c8e705e2d4ed0
Kaizen <oscar.gomes.junior@hotmail.com>
2025-05-28 20:20:37 -0300

    feat: add SignOutButton component and update authentication pages

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -3,6 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { on } from "events";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -24,7 +25,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
-import { toast } from "sonner";
 
 const LoginSchema = z.object({
   email: z.string().trim().min(1).email({
