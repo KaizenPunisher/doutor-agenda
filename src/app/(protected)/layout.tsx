cdec33757ec60a31fafe470af0a81bc90930d4b8
Kaizen <oscar.gomes.junior@hotmail.com>
2025-06-01 19:09:06 -0300

    feat: Criação de Dashboard

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,16 +2,14 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 import { AppSidebar } from "./components/app-sidebar";
 
-const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className="w-full">
         <SidebarTrigger />
         {children}
       </main>
     </SidebarProvider>
   );
-};
-
-export default ProtectedLayout;
+}
