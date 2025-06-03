e0a8c04b5453608fccfc17b1ed6dec554e84349b
Kaizen <oscar.gomes.junior@hotmail.com>
2025-06-03 13:16:11 -0300

    fix: correções eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,10 +16,7 @@ const eslintConfig = [
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
+    rules: {},
   },
 ];
 
